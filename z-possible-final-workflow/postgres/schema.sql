1249d5740c1d6449143faf0a45361c6405b743e4
Kevin Cunningham <kevin@kevincunningham.co.uk>
2025-07-01 11:37:09 +0100

    updates schema

--- z-possible-final-workflow/postgres/schema.sql
+++ z-possible-final-workflow/postgres/schema.sql
@@ -1,14 +1,11 @@
 CREATE EXTENSION IF NOT EXISTS vector;
 
-CREATE TABLE
+CREATE TABLE IF NOT EXISTS
   docs (
     id UUID PRIMARY KEY,
     pdf_id TEXT,
     page INTEGER,
     text TEXT,
-    embedding VECTOR(1024),
-    metadata JSONB,
-    start INTEGER,
-    end INTEGER
+    embedding VECTOR(1024)
   ); 
 
